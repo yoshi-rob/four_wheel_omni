e05c7955efe0ad20712d8640dc235989aecce447
yoshi-rob <95674429+yoshi-rob@users.noreply.github.com.>
2023-12-14 19:32:08 +0900

    first commit

---
+++ Arduino/FourWheelOmni/FourWheelOmni.ino
@@ -0,0 +1,97 @@
+#include "Cubic.controller.h"
+#include "PID.h"
+#include "cubic_arduino.h"
+#include <Arduino.h>
+#include <math.h>
+#include <ros.h>
+#include <std_msgs/Float32MultiArray.h>
+
+ros::NodeHandle nh;
+
+const int BAUD_RATE = 80000000;
+const uint8_t motorNum[] = {0, 1, 2, 3};
+const uint8_t incEncNum[] = {0, 1, 2, 3};
+const uint16_t INC_CPR = 1024 * 4;
+const double P = 0.8;
+const double capableDuty = 0.5;
+float velocity_target[] = {0, 0, 0, 0};
+float Kp[] = {1.0, 1.0, 1.0, 1.0};
+float Ki[] = {0.0, 0.0, 0.0, 0.0};
+float Kd[] = {0.0, 0.0, 0.0, 0.0};
+
+void wheel_vel_callback(const std_msgs::Float32MultiArray &wheel_vel_msg) {
+    velocity_target[0] = wheel_vel_msg.data[0];
+    velocity_target[1] = wheel_vel_msg.data[1];
+    velocity_target[2] = wheel_vel_msg.data[2];
+    velocity_target[3] = wheel_vel_msg.data[3];
+}
+
+std_msgs::Float32MultiArray vel_msg;
+ros::Publisher vel_pub("vel", &vel_msg);
+ros::Subscriber<std_msgs::Float32MultiArray> wheel_vel_sub("wheel_vel",
+                                                           wheel_vel_callback);
+
+void setup() {
+    Cubic::begin();
+    nh.getHardware()->setBaud(BAUD_RATE);
+    nh.initNode();
+
+    while (!nh.connected()) {
+        nh.spinOnce();
+    }
+
+    while (!nh.getParam("/Arduino/wheel/Kp", Kp, 4)) {
+        nh.logerror("Failed to get Kp");
+    }
+    while (!nh.getParam("/Arduino/wheel/Ki", Ki, 4)) {
+        nh.logerror("Failed to get Ki");
+    }
+    while (!nh.getParam("/Arduino/wheel/Kd", Kd, 4)) {
+        nh.logerror("Failed to get Kd");
+    }
+
+    vel_msg.data = (float *)malloc(sizeof(float) * 4);
+    vel_msg.data_length = 4;
+
+    nh.subscribe(wheel_vel_sub);
+    nh.advertise(vel_pub);
+}
+
+void loop() {
+    nh.spinOnce();
+    static bool stopFlag = false;
+    using namespace Cubic_controller;
+    static Velocity_PID velocityPID[] = {
+        {motorNum[0], incEncNum[0], encoderType::inc, INC_CPR, capableDuty, P,
+         Kp[0], Ki[0], Kd[0], -velocity_target[0], true, false},
+        {motorNum[1], incEncNum[1], encoderType::inc, INC_CPR, capableDuty, P,
+         Kp[1], Ki[1], Kd[1], -velocity_target[1], true, false},
+        {motorNum[2], incEncNum[2], encoderType::inc, INC_CPR, capableDuty, P,
+         Kp[2], Ki[2], Kd[2], -velocity_target[2], true, false},
+        {motorNum[3], incEncNum[3], encoderType::inc, INC_CPR, capableDuty, P,
+         Kp[3], Ki[3], Kd[3], -velocity_target[3], true, false}};
+
+    if (abs(velocity_target[0]) <= 0.001 && abs(velocity_target[1]) <= 0.001 &&
+        abs(velocity_target[2]) <= 0.001 && abs(velocity_target[3]) <= 0.001) {
+        stopFlag = true;
+    } else {
+        stopFlag = false;
+    }
+
+    if (stopFlag) {
+        for (int i = 0; i < 4; i++) {
+            DC_motor::put(motorNum[i], 0);
+            vel_msg.data[i] = velocityPID[i].getCurrent();
+        }
+        nh.loginfo("Stopping");
+    } else {
+        for (int i = 0; i < 4; i++) {
+            velocityPID[i].setTarget(velocity_target[i]);
+            velocityPID[i].compute();
+            vel_msg.data[i] = velocityPID[i].getCurrent();
+        }
+        nh.loginfo("Moving");
+    }
+    vel_pub.publish(&vel_msg);
+    Cubic::update();
+}
---
+++ src/four_wheel_omni.cpp
@@ -0,0 +1,83 @@
+#include <cmath>
+#include <geometry_msgs/Twist.h>
+#include <ros/ros.h>
+#include <std_msgs/Bool.h>
+#include <std_msgs/Float32MultiArray.h>
+
+class FourWheelOmni {
+  private:
+    ros::NodeHandle nh_;
+    ros::NodeHandle pnh_;
+    ros::Publisher wheel_pub_;
+    ros::Subscriber twist_sub_;
+    ros::Subscriber safe_signal_sub_;
+    ros::Timer timer_;
+    geometry_msgs::Twist last_twist_;
+    bool safe_signal = false;
+
+  public:
+    FourWheelOmni() : nh_(), pnh_("~") {
+        wheel_pub_ = nh_.advertise<std_msgs::Float32MultiArray>("wheel_vel", 1);
+        twist_sub_ =
+            nh_.subscribe("cmd_vel", 1, &FourWheelOmni::twistCallback, this);
+        safe_signal_sub_ = nh_.subscribe(
+            "safe_signal", 1, &FourWheelOmni::safeSignalCallback, this);
+        timer_ = nh_.createTimer(ros::Duration(0.1),
+                                 &FourWheelOmni::timerCallback, this);
+    }
+
+    void twistCallback(const geometry_msgs::Twist &twist_msg) {
+        last_twist_ = twist_msg;
+    }
+
+    void safeSignalCallback(const std_msgs::Bool &safe_signal_msg) {
+        safe_signal = safe_signal_msg.data;
+    }
+
+    void timerCallback(const ros::TimerEvent &e) {
+        std_msgs::Float32MultiArray wheel_vel;
+        wheel_vel.data.resize(4);
+        if (safe_signal == false) {
+            ROS_DEBUG("safe_signal is false");
+            wheel_vel.data[0] = 0;
+            wheel_vel.data[1] = 0;
+            wheel_vel.data[2] = 0;
+            wheel_vel.data[3] = 0;
+        } else {
+            float vx = last_twist_.linear.x;
+            float vy = last_twist_.linear.y;
+            float vz = last_twist_.angular.z;
+
+            // オムニホイールの角度
+            float theta1 = M_PI / 4.0;
+            float theta2 = 3.0 * M_PI / 4.0;
+            float theta3 = 5.0 * M_PI / 4.0;
+            float theta4 = 7.0 * M_PI / 4.0;
+
+            // オムニホイールの速度計算
+            wheel_vel.data[0] =
+                vx * cos(theta1) + vy * sin(theta1) + vz; // 右前輪
+            wheel_vel.data[1] =
+                vx * cos(theta2) + vy * sin(theta2) + vz; // 左前輪
+            wheel_vel.data[2] =
+                vx * cos(theta3) + vy * sin(theta3) + vz; // 左後輪
+            wheel_vel.data[3] =
+                vx * cos(theta4) + vy * sin(theta4) + vz; // 右後輪
+        }
+
+        wheel_pub_.publish(wheel_vel);
+    }
+};
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "four_wheel_omni");
+    FourWheelOmni four_wheel_omni;
+    ros::Rate loop_rate(10);
+
+    while (ros::ok()) {
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+
+    return 0;
+}
